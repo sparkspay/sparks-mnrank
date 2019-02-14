5f7600999e89cd4d158cca61c9c9fd075b1861a0
m0r4k <mor4k@outlook.com>
2019-02-14 20:55:53 +0100

    setup.py initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sparks-mnrank',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/sparkspay/sparks-mnrank',
+    license='MIT',
+    author='m0r4k',
+    author_email='m0r4k@outlook.com',
+    description='Sparks RankViewer'
+)
